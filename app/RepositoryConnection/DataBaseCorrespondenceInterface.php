5cc573f9424ed5408e539f174cddb9efa56a9541
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-05-12 09:58:24 -0300

    Add method to retur prothected or private property from an model class

--- app/RepositoryConnection/DataBaseCorrespondenceInterface.php
+++ app/RepositoryConnection/DataBaseCorrespondenceInterface.php
@@ -4,4 +4,5 @@ namespace DataBase\RepositoryConnection;
 
 interface DataBaseCorrespondenceInterface {
     public static function getTable();
+    public function getProperty(string $propertyName);
 }
